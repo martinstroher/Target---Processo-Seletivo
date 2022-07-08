a1ff48ec08083db698b83f78e6ba56c6d8ec6092
Martin Stroher <martinstrev@gmail.com>
2022-07-08 15:59:15 -0300

    Atualização e organização do programa 3

--- src/Program3.java
+++ src/Program3.java
@@ -8,18 +8,22 @@ public class Program3 {
 
 	public static void main(String[] args) {
 		List<Dia> listaDeDias = new ArrayList<Dia>();
+		
 		List<Double> listaDeFaturamentos = Arrays.asList(22174.1664, 24537.6698, 26139.6134, 0.0, 0.0,
 				26742.6612,	0.0, 42889.2258, 46251.174,	11191.4722,	0.0, 0.0, 3847.4823, 373.7838,
 				2659.7563, 48924.2448, 18419.2614, 0.0, 0.0, 35240.1826, 43829.1667, 18235.6852, 4355.0662,
 				13327.1025, 0.0, 0.0, 25681.8318, 1718.1221, 13220.495, 8414.61);
+		
+		
 		int i = 1;
 		for (Double faturamento : listaDeFaturamentos) {
 			listaDeDias.add(new Dia(i, faturamento));
 			i++;
 		}
+		
+		
 		Double menorValor = 999999.0;
 		Double maiorValor = 0.0;
-		
 		for (Dia dia : listaDeDias)
 			if (dia.getFaturamento() < menorValor && dia.getFaturamento() != 0.0) {
 				menorValor = dia.getFaturamento();
@@ -30,9 +34,14 @@ public class Program3 {
 				maiorValor = dia.getFaturamento();
 			}
 		
+		
+		
 		List<Dia> listaDeDiasUteis = listaDeDias.stream().filter(x -> x.getFaturamento() != 0.0).collect(Collectors.toList());
 		double faturamentoTotal = listaDeDiasUteis.stream().map(x -> x.getFaturamento()).reduce(0.0, (x, y) -> x + y);
 		double mediaMensal = faturamentoTotal/ listaDeDiasUteis.size();
+		
+		
+		
 		
 		List<Dia> listaAltoFaturamento = listaDeDiasUteis.stream().filter(x -> x.getFaturamento() > mediaMensal).collect(Collectors.toList());
 		System.out.println("====Dados do mês====");
